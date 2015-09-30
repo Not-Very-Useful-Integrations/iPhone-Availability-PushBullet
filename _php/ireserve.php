77eaf283f33b9c78bbde08485cf5c9ff56f3204b
Thomas Tong <mankintong@gmail.com>
2015-09-30 23:00:24 +0800

    Update ireserve.php

--- _php/ireserve.php
+++ _php/ireserve.php
@@ -6,21 +6,21 @@ $phone_def = array(
 	array('MKU52ZP/A','5.5 rose 16gb'),
 	array('MKU92ZP/A','5.5 rose 64gb'),
 	array('MKUG2ZP/A','5.5 rose 128gb'),
-	array('MKQM2ZP/A','4.7 rose 16gb'),
-	array('MKQR2ZP/A','4.7 rose 64gb'),
-	array('MKQW2ZP/A','4.7 rose 128gb'),
-	array('MKU32ZP/A','5.5 gold 16gb'),
-	array('MKU82ZP/A','5.5 gold 64gb'),
-	array('MKUF2ZP/A','5.5 gold 128gb'),
+	// array('MKQM2ZP/A','4.7 rose 16gb'),
+	// array('MKQR2ZP/A','4.7 rose 64gb'),
+	// array('MKQW2ZP/A','4.7 rose 128gb'),
+	// array('MKU32ZP/A','5.5 gold 16gb'),
+	// array('MKU82ZP/A','5.5 gold 64gb'),
+	// array('MKUF2ZP/A','5.5 gold 128gb'),
 	// array('MKQL2ZP/A','4.7 gold 16gb'),
 	// array('MKQQ2ZP/A','4.7 gold 64gb'),
 	// array('MKQV2ZP/A','4.7 gold 128gb'),
 	// array('MKU22ZP/A','5.5 silver 16gb'),
 	// array('MKU72ZP/A','5.5 silver 64gb'),
-	array('MKUE2ZP/A','5.5 silver 128gb'),
+	// array('MKUE2ZP/A','5.5 silver 128gb'),
 	// array('MKU12ZP/A','5.5 gray 16gb'),
 	// array('MKU62ZP/A','5.5 gray 64gb'),
-	array('MKUD2ZP/A','5.5 gray 128gb')
+	// array('MKUD2ZP/A','5.5 gray 128gb')
 	// array('MKQK2ZP/A','4.7 silver 16gb'),
 	// array('MKQP2ZP/A','4.7 silver 64gb'),
 	// array('MKQU2ZP/A','4.7 silver 128gb'),
@@ -29,7 +29,7 @@ $phone_def = array(
 	// array('MKQT2ZP/A','4.7 gray 128gb')
 );
 
-$pushKeys = array();
+$pushKeys = array('v16KqXNi5pRkpQUX47mXZ3iuTMcQFmNAZoujxBr1bny5k', 'v1D50EttDgOqwNaRmmLccqDPRbLgtef2oLujxOSx9N7n2', 'BgYmPH6NuelCe8sK2k4OVn0MITEaeecm');
 
 $pushUrl = 'https://api.pushbullet.com/v2/pushes';
 $debug = false || ($_GET['debug'] == 1);
